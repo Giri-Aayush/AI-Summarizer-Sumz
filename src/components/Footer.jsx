f562709c7346d5a3f35a4bb98767ccd78106fb23
Giri-Aayush <aayushgiri1234@gmail.com>
2023-07-02 17:31:03 +0530

    AI-Sumz-done!

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className='pt-10 mb-5'>
+      <p>Made with ♥ Aayush Giri</p>
+    </footer>
+  );
+};
+
+export default Footer;
